f688ae1dced9619da0b837f1e16c1b0e59131a08
eeylul <89338535+eeylul@users.noreply.github.com>
2022-04-26 23:53:44 +0300

    Add files via upload
    Dİe benötigte SQL-Verbindung Klassen hergestellt. 
    Note: Es gibt einige Fehler. Vor dem nachsten Abgeben wird das korrigiert.

---
+++ Connection.java
@@ -0,0 +1,20 @@
+package java.sql;
+
+public class Connection {
+
+	public DatabaseMetaData getMetaData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isClosed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ DriverManager.java
@@ -0,0 +1,15 @@
+package java.sql;
+
+public class DriverManager {
+
+	public static Connection getConnection(String url, String user, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Connection getConnection(String conURL) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
